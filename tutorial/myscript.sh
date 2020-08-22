eb673e7b794b5232483f7c51c8a014cb3dad467e
hannah <hannah@hannahdeMacBook-Pro.local>
2020-08-22 11:40:05 -0400

    add Readme and myscript

---
+++ tutorial/myscript.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+echo "------script startup-----"
+echo "------list all file------"
+ls ~/
+
+echo "------find readme------"
+find ~ -iname "readme*"
+
+
+echo "------script shutdown-----"
